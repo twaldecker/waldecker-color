a5be84bc0c3bff05ffedeafc033ed70688d0cd83
Thomas Waldecker <waldecker@qurdo.com>
2020-03-14 16:08:59 +0100

    Demo: add demo files.

---
+++ demo/classfile.ts
@@ -0,0 +1,13 @@
+import moment from 'moment'
+
+class TheClass extends Foo {
+    public x
+
+    // the comment
+    public func () {
+        if (true) {
+
+        }
+        return this.x;
+    }
+}
